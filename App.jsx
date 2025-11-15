aecd351459891abd538388202802574ee3d866fa
scar9031112003 <scarlettsalazar43@gmail.com>
2025-11-14 22:02:39 -0500

    Modificaciones del APP

---
+++ App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Games from "./pages/games/Games";
+import AddGame from "./pages/addgame/AddGame";
+import Reviews from "./pages/reviews/Reviews";
+import AddReview from "./pages/addreview/AddReview";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/add-game" element={<AddGame />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/add-review" element={<AddReview />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
